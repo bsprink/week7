ee1a72cecefa5b531444941c54289ab4441f0128
bsprink <bsprink@comcast.net>
2017-02-25 12:00:27 -0600

    got some things moving

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,33 @@
 //my scripts
 
-$(document).scroll(function(){
-	console.log(
-			$(document).scrollTop()
-		);
+//$(document).scroll(function(){
+	//console.log(
+			//$(document).scrollTop()
+		//);
 
-	$("#frontClouds").css("left",String($(document).scrollTop() -70) + "px" );
-	$("#middleClouds").css("left",String($(document).scrollTop() * .5) + "px" );
-	$("#backClouds").css("left",String($(document).scrollTop() * .2) + "px" );
+	//$("#frontClouds").css("left",String($(document).scrollTop() -70) + "px" );
+	//$("#middleClouds").css("left",String($(document).scrollTop() * .5) + "px" );
+	//$("#backClouds").css("left",String($(document).scrollTop() * .2) + "px" );
 	//$("#me").css("left",String($(document).scrollTop() * .2) + "px" );
-});
-
-$(document).mousemove(function(p){
-	console.log(p.pageX + "," +p.pageY);
-	if(p.pageX > 100 && p.pageX < 900){
-		$("#frontClouds").css("left",String(p.pageX * -.1) + "px" );
-		$("#middleClouds").css("left",String(p.pageX * .0 ) + "px" );
-		$("#backClouds").css("left",String(p.pageX * .0) + "px" );
+//
+//$(document).mousemove(function(p){
+	//console.log(p.pageX + "," +p.pageY);
+	//if(p.pageX > 100 && p.pageX < 900){
+		//$("#frontClouds").css("left",String(p.pageX * -.1) + "px" );
+		//$("#middleClouds").css("left",String(p.pageX * .0 ) + "px" );
+		//$("#backClouds").css("left",String(p.pageX * .0) + "px" );
 		//$("#me").css("left",String(p.pageX * .100) + "px" );
-	}
-});
+	//}
+//});
 
 $(document).scroll(function(){
 	console.log($(document).scrollTop());
 
-	$("#row1").css("right", String($(document).scrollTop() - 1000) +"px");
-
+	$("#trees").css("right", String($(document).scrollTop() - 3720) +"px");
+	$("#grass").css("right", String($(document).scrollTop() - 3700) +"px");
+	$("#hills").css("right", String($(document).scrollTop() - 3700) +"px");
+	//$("#sky").css("right", String($(document).scrollTop() - 1000) +"px");
+	
 	if( $ (document).scrollTop() > 0){
 		$("#row2").css("left", String ($(document).scrollTop() - 1000) + "px");
 	}
@@ -36,27 +38,27 @@ var animationCount = 0;
 var guyMoving = false;
 var moveGuyTimer; 
 
-setInterval(showNum, 246);
+
 
 function showNum(){
 	animationCount ++;
-	if(animationCount > 4){
+	if(animationCount > 3){
 		animationCount = 0;
-		clearInterval(moveGuyTimer);
-		guyMoving = false;
+		//clearInterval(moveGuyTimer);
+		//guyMoving = false;
 	}
-	if(animationCount == 1){
+	//if(animationCount == 1){
 
-		document.getElementById("me").style.backgroundPosition = "0px 0px";
-	} 
-	else if(animationCount == 2){
-		document.getElementById("me").style.backgroundPosition = "-114px 0px";
+		
+	//} 
+	/*else*/if(animationCount == 1){
+		document.getElementById("me").style.backgroundPosition = "-120px 0px";
+
+	} else if(animationCount == 2){
+		document.getElementById("me").style.backgroundPosition = "-230px 0px";
 
 	} else if(animationCount == 3){
-		document.getElementById("me").style.backgroundPosition = "-214px 0px";
-
-	} else if(animationCount == 4){
-		document.getElementById("me").style.backgroundPosition = "-314px 0px";
+		document.getElementById("me").style.backgroundPosition = "-335px 0px";
 	}
 }
 
@@ -65,5 +67,14 @@ $(document).scroll(function(){
 		moveGuyTimer = setInterval(showNum, 246);
 		guyMoving = true;
 	}
-	
+	$(document).mouseup(function(){
+		document.getElementById("me").style.backgroundPosition = "0px 0px";
+		clearInterval(moveGuyTimer);
+		guyMoving = false;
+	});
+		$(document).keyup(function(){
+		document.getElementById("me").style.backgroundPosition = "0px 0px";
+		clearInterval(moveGuyTimer);
+		guyMoving = false;
+	});
 });
