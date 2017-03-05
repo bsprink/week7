f1449d02c04bc417a34a0eb5c97192574f888b81
bsprink <bsprink@comcast.net>
2017-03-04 22:33:44 -0600

    can't figure out how to move text like I want

--- scripts/main.js
+++ scripts/main.js
@@ -10,10 +10,20 @@ $(document).scroll(function(){
 	$("#trees").css("left", String($(document).scrollTop() * -.3) +"px");
 	$("#moon").css("top", String($(document).scrollTop() * .1) +"px");
 	$("#sky").css("right", String($(document).scrollTop() * .3) +"px");
-	$("#stars").css("right", String($(document).scrollTop() *.3)  +"px");
-	$("#sun").css("left", String($(document).scrollTop()  * .1 ) +"px");
+	$("#stars").css("right", String($(document).scrollTop() * .3)  +"px");
+	$("#name").css("top", String($(document).scrollTop() * .09) +"px");
+	$("#introText").css("top",  String($(document).scrollTop() * .08) +"px");
+	//$("#edu").css("top", String($(document).scrollTop() * .07) +"px");
+	//$("#eduText").css("top",  String($(document).scrollTop() * .06) +"px");
+	//$("#workHist").css("top", String($(document).scrollTop() * .05) +"px");
+	//$("#workHistText").css("top",  String($(document).scrollTop() * .04) +"px");
+	//$("#contact").css("top",  String($(document).scrollTop() * .03) +"px");
+	//$("#email").css("top",  String($(document).scrollTop() * .02) +"px");
+	
 	
 });
+	
+
 
 var animationCount = 0;
 var guyMoving = false;
